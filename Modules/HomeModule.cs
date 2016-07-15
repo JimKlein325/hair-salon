0d7ad0ff8d56e792b6c568b5a8de4f4b42902bd1
Jim Klein <j.mesklein@gmail.com>
2016-07-15 12:01:44 -0700

    add routes for Stylist Save and Delete functionality.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Nancy;
 using Nancy.ViewEngines.Razor;
 using HairSalon.Objects;
+using System;
 
 namespace HairSalon
 {
@@ -10,7 +11,20 @@ namespace HairSalon
     public HomeModule()
     {
       Get["/"]= _ => {
-        return View["index.cshtml"];
+        List<Stylist> stylists = Stylist.GetAll();
+        return View["index.cshtml", stylists];
+      };
+      Post["/"] = _ => {
+        Stylist stylist = new Stylist(Request.Form["stylist-name"]);
+        stylist.Save();
+        List<Stylist> stylists = Stylist.GetAll();
+        return View["index.cshtml", stylists];
+      };
+      Delete["stylists/delete/{id}"] = paramaters => {
+        Stylist stylist = Stylist.Find(paramaters.id);
+        Console.WriteLine("param: " + paramaters.id + " Stylist.id: " + stylist.GetId());
+        stylist.Delete();
+        return View["index.cshtml", Stylist.GetAll()];
       };
 
     }
